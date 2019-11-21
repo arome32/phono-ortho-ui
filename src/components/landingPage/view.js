5341e0a6bf8322b353a8302094a16eba2dc9e993
Alex Romero <aromero32@email.arizona.edu>
2019-11-20 23:51:01 -0700

    updated the landing page with the new logo, also created and added a new favicon

--- src/components/landingPage/view.js
+++ src/components/landingPage/view.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {withRouter} from 'react-router';
-import logo from '../../assets/images/logo.svg'
+import logo from '../../assets/images/ts-logo.svg'
 import { WhiteTextField } from '../../assets/inputTypes/inputTypes'
 import Button from '@material-ui/core/Button';
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
